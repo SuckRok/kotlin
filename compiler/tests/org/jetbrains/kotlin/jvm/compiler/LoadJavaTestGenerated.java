751da1d099d15197c87b9f265dc46981f444da0d
Denis Zharkov <denis.zharkov@jetbrains.com>
2015-08-03 12:50:13 +0300

    Do not create redundant special variance when enhancing types
     #KT-8538 Fixed

--- compiler/tests/org/jetbrains/kotlin/jvm/compiler/LoadJavaTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/jvm/compiler/LoadJavaTestGenerated.java
@@ -1376,6 +1376,12 @@ public class LoadJavaTestGenerated extends AbstractLoadJavaTest {
                 String fileName = JetTestUtils.navigationMetadata("compiler/testData/loadJava/compiledJava/mutability/LoadIterableWithPropagation.java");
                 doTestCompiledJava(fileName);
             }
+
+            @TestMetadata("ReadOnlyExtendsWildcard.java")
+            public void testReadOnlyExtendsWildcard() throws Exception {
+                String fileName = JetTestUtils.navigationMetadata("compiler/testData/loadJava/compiledJava/mutability/ReadOnlyExtendsWildcard.java");
+                doTestCompiledJava(fileName);
+            }
         }
 
         @TestMetadata("compiler/testData/loadJava/compiledJava/notNull")
